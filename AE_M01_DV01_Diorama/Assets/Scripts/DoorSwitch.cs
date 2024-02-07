72879c09467d71adf91b20a028a534a31a8fde1d
allikitten <allison.evdokimo@cox.net>
2024-02-07 15:54:22 -0700

    :)

--- AE_M01_DV01_Diorama/Assets/Scripts/DoorSwitch.cs
+++ AE_M01_DV01_Diorama/Assets/Scripts/DoorSwitch.cs
@@ -6,10 +6,13 @@ public class DoorSwitch : MonoBehaviour
 {
     public GameObject Door;
 
+    public Renderer ren;
+
     bool isOpen = false;
     private void OnTriggerEnter(Collider collider)
     {
         Debug.Log("Door open");
+        ren.material.color = Color.green;
         isOpen = true;
         Door.transform.position += new Vector3(0, 5, 0);
     }
